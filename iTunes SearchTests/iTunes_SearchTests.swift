6a97b0eb5bcc667641c4c3ec6519065f4431e310
Mitchell Budge <mitchellbudge@gmail.com>
2019-10-14 10:45:57 -0600

    Configure expectation and Unit Test

--- iTunes SearchTests/iTunes_SearchTests.swift
+++ iTunes SearchTests/iTunes_SearchTests.swift
@@ -7,27 +7,20 @@
 //
 
 import XCTest
+@testable import iTunes_Search
 
 class iTunes_SearchTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testForSomeResult() {
+        let controller = SearchResultController()
+        let resultsExpectation = expectation(description: "Wait for results")
+        
+        controller.performSearch(for: "GarageBand", resultType: .software) {
+            resultsExpectation.fulfill()
         }
+        wait(for: [resultsExpectation], timeout: 2)
+        
+        XCTAssertTrue(controller.searchResults.count > 0, "Expecting at least one result for GarageBand")
     }
-
+   
 }
